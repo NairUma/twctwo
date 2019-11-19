7fd1f6cc66e1500e5bae75dc9209e2548ff260a3
NairUma <umanair2000@gmail.com>
2019-11-19 13:38:23 -0800

    adding files and testing things

--- main.py
+++ main.py
@@ -153,25 +153,34 @@ class FastPage(webapp2.RequestHandler):
         # self.response.write(results_template.render(the_variable_dict))
         pass
 
-class MapPage(webapp2.RequestHandler):
+class ColePage(webapp2.RequestHandler):
     def get(self):
-        map_template = the_jinja_env.get_template('templates/map1.html')
-        self.response.headers['Content-Type'] = 'html'
-        the_variable_dict = {
-            "img_url": "/css/images/MAPS/rawmap.png"
-        }
-        self.response.write(map_template.render())
-
-class AboutPage(webapp2.RequestHandler):
-    def get(self):
-        about_template = the_jinja_env.get_template('templates/about.html')
+        about_template = the_jinja_env.get_template('templates/cole.html')
         self.response.headers['Content-Type'] = 'html'
         self.response.write(about_template.render())
 
+class CharliePage(webapp2.RequestHandler):
+    def get(self):
+        about_template = the_jinja_env.get_template('templates/charlie.html')
+        self.response.headers['Content-Type'] = 'html'
+        self.response.write(about_template.render())
+
+class LivPage(webapp2.RequestHandler):
+    def get(self):
+        about_template = the_jinja_env.get_template('templates/liv.html')
+        self.response.headers['Content-Type'] = 'html'
+        self.response.write(about_template.render())
+
+class AspenPage(webapp2.RequestHandler):
+    def get(self):
+        about_template = the_jinja_env.get_template('templates/aspen.html')
+        self.response.headers['Content-Type'] = 'html'
+        self.response.write(about_template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/fastestpath', FastPage),
-    ('/map', MapPage),
-    ('/about', AboutPage)
+    ('/liv', LivPage),
+    ('/cole', ColePage),
+    ('/charlie', CharliePage),
+    ('/aspen', AspenPage)
 ], debug=True)
